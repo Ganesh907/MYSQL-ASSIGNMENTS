560ce313becf6774adfde30569e627ddba9c0fec
Ganesh907 <97492958+Ganesh907@users.noreply.github.com>
2022-03-11 16:29:46 +0530

    Create Assignment1.sql

---
+++ Assignment1.sql
@@ -0,0 +1,8 @@
+create table CUSTOMER (Cust_id Int (5),Fname Varchar2 (15),Lname Varchar2(15),Area Char (2),Phone Number (10),DOB Date,Payment Number 
+(6,2)PRIMARY KEY (Cust_id) );
+insert into CUSTOMER values( 11,'Ivan',' Ross',' SA',61254,1987-01-15, 800.50);
+insert into CUSTOMER(12,' Vandana', 'Ray', 'MU','5560379, 2021-3-17, 1000.75);
+insert into CUSTOMER(13,'Pramada,' Jauguste ','DA', 4560389,25-jul-67, 500.00);
+insert into CUSTOMER(14,'Basu',' Navindi',' BA', 6125401,30-feb-56, 860.00);
+insert into CUSTOMER(15,' Ravi',' Shridhar','NA',,15-feb-89, 500.50);
+insert into CUSTOMER(16,'Rukmini',' Aiyer',' GH', 5125274, 23-jul-87,1500.50);
