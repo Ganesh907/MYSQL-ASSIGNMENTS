caaaa0c9f5c114140a605dfd866982a04b838293
Ganesh907 <97492958+Ganesh907@users.noreply.github.com>
2022-03-14 14:57:02 +0530

    Update Assignment1.sql

--- Assignment1.sql
+++ Assignment1.sql
@@ -1,11 +1,11 @@
 create table CUSTOMER (Cust_id Int (5),Fname Varchar2 (15),Lname Varchar2(15),Area Char (2),Phone Number (10),DOB Date,Payment Number 
 (6,2)PRIMARY KEY (Cust_id) );
 insert into CUSTOMER values( 11,'Ivan',' Ross',' SA',61254,1987-01-15, 800.50);
-insert into CUSTOMER(12,' Vandana', 'Ray', 'MU','5560379, 2021-3-17, 1000.75);
-insert into CUSTOMER(13,'Pramada,' Jauguste ','DA', 4560389,25-jul-67, 500.00);
-insert into CUSTOMER(14,'Basu',' Navindi',' BA', 6125401,30-feb-56, 860.00);
-insert into CUSTOMER(15,' Ravi',' Shridhar','NA',,15-feb-89, 500.50);
-insert into CUSTOMER(16,'Rukmini',' Aiyer',' GH', 5125274, 23-jul-87,1500.50);
+insert into CUSTOMER values (12,' Vandana', 'Ray', 'MU','5560379, 2021-3-17, 1000.75);
+insert into CUSTOMER values (13,'Pramada',' Jauguste ','DA', 4560389,25-jul-67, 500.00);
+insert into CUSTOMER values (14,'Basu',' Navindi',' BA', 6125401,30-feb-56, 860.00);
+insert into CUSTOMER values (15,' Ravi',' Shridhar','NA',,15-feb-89, 500.50);
+insert into CUSTOMER values (16,'Rukmini',' Aiyer',' GH', 5125274, 23-jul-87,1500.50);
 desc CUSTOMER;
 
 select * from CUSTOMER;
@@ -32,9 +32,9 @@ truncate CUSTOMER;
 
 insert into CUSTOMER values( 11,'Ganesh', 'Shinde',' yd',73822829,1987-01-15, 732.50);
 
-insert into CUSTOMER(12,' Rahul', 'Shinde', 'ut',6383848, 1983-3-17, 840.75);
+insert into CUSTOMER values (12,' Rahul', 'Shinde', 'ut',6383848, 1983-3-17, 840.75);
 
-insert into CUSTOMER(13,'Ajay,'Bagh','jd', 263737,25-jul-67, 80.00);
+insert into CUSTOMER values (13,'Ajay','Bagh','jd', 263737,25-jul-67, 80.00);
 
 insert into CUSTOMER(14,'Atul',' More',' ka', 484848,30-feb-56, 190.00);
 
